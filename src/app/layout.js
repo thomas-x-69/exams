eb53fc23e8e2a4d93aa1fe992abdcdd2bd8c4028
thomas-x-69 <thomas.ashraf975@gmail.com>
2025-04-18 21:03:41 +0200

    removed popunder

--- src/app/layout.js
+++ src/app/layout.js
@@ -156,7 +156,7 @@ export default function RootLayout({ children }) {
           type="text/javascript"
           src="//resolvedinsaneox.com/55/0f/6e/550f6e2624c4b06afeb9e2c9270717f9.js"
         ></Script> */}
-        <Script>
+        {/* <Script>
           {`
     // Only load popunder after user has been on the site for 30 seconds
     setTimeout(() => {
@@ -166,7 +166,7 @@ export default function RootLayout({ children }) {
       document.head.appendChild(script);
     }, 10000);  // 10 seconds
   `}
-        </Script>
+        </Script> */}
       </head>
 
       <body
